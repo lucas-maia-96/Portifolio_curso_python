f430217e152e654fa94e77d718b3c5a1e891b6fa
lucas_home <lucasmais@dcc.ufmg.br>
2023-07-27 14:37:51 -0300

    Create Columns and personal info

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/luke.jpg")
+    
+with col2:
+    st.title("Lucas Maia")
+    content = """ Olá, meu nome é Lucas, tenho 26 anos e sou estudante de Sistemas de Informação, além de ser graduado em Matemática. Tenho uma profunda paixão por aprendizado contínuo e sou fascinado pelo poder da ciência dos dados. Meu interesse principal está na área de Ciência de Dados, abrangendo tópicos como Machine Learning, Redes Neurais, Inteligência Artificial e Deep Learning. Estou sempre em busca de oportunidades para aprimorar minhas habilidades e contribuir para projetos desafiadores nesse campo emocionante. """
+    st.info(content)
